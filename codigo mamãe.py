5672c9b29d90857070284a6eeae18d5fa6d6bc58
Airon <cavalcanteairon32@gmail.com>
2025-05-05 13:12:11 -0300

    updated

---
+++ codigo mamãe.py
@@ -0,0 +1,36 @@
+from time import sleep
+
+mensagem = [
+    "\033[31mFeliz dias das mães minha querida e amada mãe\033[m",
+    "\033[31m♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡\033[m",
+    "\033[31mAgradeço por toda a força e ajuda que a senhora tem me dado ao longo desses anos\033[m",
+    "\033[31m♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡\033[m",
+    "\033[31mEspero que esse dia seja repleto de luiz e amor\033[m",
+    "\033[31mVoce merece todo o amor desse mundo\033[m",
+    "\033[31mObrigado por ser essa mãe tão incrivel e maravilhosa\033[m",
+    "\033[31mEu te amo\033[m"
+]
+for linha in mensagem:
+    print(linha)
+    sleep(2.0)
+
+print()
+print("\033[31m    ***   ***\033[m")
+sleep(1.5)
+print("\033[31m  ****** ******\033[m")
+sleep(1.5)
+print("\033[31m ***************\033[m")
+sleep(1.5)
+print("\033[31m  *************\033[m")
+sleep(1.5)
+print("\033[31m   ***********\033[m")
+sleep(1.5)
+print("\033[31m    *********\033[m")
+sleep(1.5)
+print("\033[31m     *******\033[m")
+sleep(1.5)
+print("\033[31m      *****\033[m")
+sleep(1.5)
+print("\033[31m       ***\033[m")
+sleep(1.5)
+print("\033[31m        *\033[m")
